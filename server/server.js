04dc760114a576a3023f28fec5a4fcafb44741c7
alessandro.bozzi <a.bozzi@atipico.studio>
2023-04-13 12:51:31 +0200

    test

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.post('/', async (req, res) => {
         })
     } catch (error) {
         console.log(error);
-        res.status(500).send({error.message});
+        res.status(500).send({error});
     }
 });
 
